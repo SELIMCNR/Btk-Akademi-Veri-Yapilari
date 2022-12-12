28e1e9d382f05ddd33815ab005b512d154b13646
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:54:28 +0300

    C#-Algorıthm
    C#-Algorıthm

---
+++ 23-List/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _23_List
+{
+    
+    public class Araba<T> : IEnumerable //kalıtımdan devir alır.
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException(); //ınterface tasarladık
+        }
+    }
+
+    
+    
+    public interface ISehır  //Interface kontrattır
+    {
+        void Tanit();
+        void NüfusBilgisiGetir(int plakaNo);
+
+    }
+    public class Sehir : IComparable <Sehir>
+    {   
+
+        public Sehir(int plakaNo,string sehirAdi) //default constractor
+        {
+            PlakaNo = plakaNo;
+            SehirAdi = sehirAdi;
+        }
+
+
+        public int PlakaNo { get; set; }
+        public string SehirAdi { get; set; }
+
+        public int CompareTo(Sehir other) // ikinci interface burada
+        {
+            if (this.PlakaNo < other.PlakaNo)
+            {
+                return -1;
+            }
+            else if(this.PlakaNo== other.PlakaNo)
+            {
+                return 0;
+            }
+            else { return 1; }
+        }
+
+       
+
+        public override string ToString()
+        {
+            return $"{PlakaNo,-5},{SehirAdi,-15}";
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List
+            var sayilar = new List<int>() { 53,40,14,2,3,12,15}; //Liste ekledik
+       
+            sayilar.Sort(); //Sıralama metodu
+            sayilar.ForEach(s => Console.WriteLine(s));
+
+            //Sehir Listesi
+            var sehirler = new List<Sehir>()
+            {
+                new Sehir(46,"Kahramanmaraş"),
+                new Sehir(6,"Ankara"),
+                new Sehir(34,"İstanbul"),
+                new Sehir(23,"Elazığ"),
+                new Sehir(44,"Malatya"),
+            };
+            sehirler.Add(new Sehir(1, "Adana"));
+            sehirler.Sort(); //Sıralama metodu başarızı oldu compare to ile ayarlandı 
+            sehirler.ForEach(s => Console.WriteLine(s));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 24-Stack/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _24_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack ile sayının basamaklara ayrılması
+            Console.WriteLine("Lütfen bir sayı giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            var sayiYigini = new Stack<int>();
+            while (sayi > 0)
+            {
+                int k = sayi % 10;
+                sayiYigini.Push(k);
+                sayi = sayi / 10;
+            }
+
+            int i = 0;
+            int n = sayiYigini.Count - 1;
+            foreach (int s in sayiYigini)
+            {
+                Console.WriteLine($"{s,6} * {Math.Pow(10,n-i),7} = {s*Math.Pow(10,n-i),8}" );
+                i++;
+            }
+
+           karakterYığınSeti();
+            GenericStackÖrnek();
+            Console.ReadKey();
+        }
+
+        private static void karakterYığınSeti()
+        {
+            var karakterYigini = new Stack<char>();
+            for (int i = 65; i <= 90; i++)
+            {
+                karakterYigini.Push((char)(i));
+                Console.WriteLine($"{karakterYigini.Peek()} yığına eklendi.");
+                Console.WriteLine($"Yığındaki eleman sayisi : {karakterYigini.Count}");
+            }
+            //Ek bilgi 
+            var dizi = karakterYigini.ToArray(); // stack diziye çevrildi.
+            Console.WriteLine("Yığından çıkartma işlemi için bir tuşa basın");
+            while (karakterYigini.Count > 0)
+            {
+                Console.WriteLine($"{karakterYigini.Pop()} yığından çıkarıldı.");
+                Console.WriteLine($"Yığındaki eleman sayisi : {karakterYigini.Count}");
+            }
+        }
+
+        private static void GenericStackÖrnek()
+        {
+            // Stack yığın yapısı
+            //Last-in  , First out , son gelen ilk çıkar. Farklı push,pop,peek,clear,count,ToArray lerle çalışır. 
+
+            //Stack Tanımlama
+            var karakterYigini = new Stack<char>(); // system-collection-genericten stack oluşumu 
+
+            //Ekleme
+            karakterYigini.Push('A');
+            Console.WriteLine(karakterYigini.Peek()); /* hangi ifadelerin eklendini gösterir*/
+            karakterYigini.Push('B');
+            Console.WriteLine(karakterYigini.Peek());
+            karakterYigini.Push('C');
+            Console.WriteLine(karakterYigini.Peek());
+            karakterYigini.Push('D');
+
+            //Çıkarma 
+            Console.WriteLine(karakterYigini.Pop() + "yığından çıkarıldı.");
+            Console.WriteLine(karakterYigini.Pop() + "yığından çıkarıldı.");
+            Console.WriteLine(karakterYigini.Pop() + "yığından çıkarıldı.");
+
+        }
+    }
+}
