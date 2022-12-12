f526ff8a8a743435088a5864d8a8887036028335
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:55:40 +0300

    C#-Algorıthm
    C#-Algorıthm

---
+++ 27-Dictionary/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _26_Dictionary
+{
+    internal class Program
+    {
+        public class Personel
+        {
+            public Personel(string name, string surname, decimal mass)
+            {
+                Name = name;
+                Surname = surname;
+                Mass = mass;
+            }
+
+            public int SicilNo { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            public decimal Mass { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name,-10} {Surname,-15} {Mass,-10}";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Dictionary
+            var personelListesi = new Dictionary<int, Personel>();
+            personelListesi.Add(100, new Personel("Selim", "Çınar", 10000));
+
+            //Tanım2
+            var personelListesi2 = new Dictionary<int, Personel>()
+            {
+                {110, new Personel("Mehmet","Sonsoz",7500) },
+                {120, new Personel("Ahmet","Can",9000) }
+            };
+
+
+            //dolaşma
+            foreach (var p in personelListesi)
+            {
+                Console.WriteLine(p);
+            }
+
+            DictionaryTemel();
+            Console.ReadKey();
+        }
+
+        private static void DictionaryTemel()
+        {
+            //Dictionary <TKey,TValue 
+            // System-collections-generic sınıfı altında yer alır.
+            // boxing->unboxing yok system generic sınıfında
+            // Tkey - Tvalue ,anahtar -değer çifti 
+            //Tkey benzersiz -> Tvalue istediğiniz  
+            // Hashtable göre daha performanslı çalışır.
+
+            //Dictionary tanım 
+            var telefonKodlari = new Dictionary<int, string>();
+
+
+            //Dictionary tanım 2 
+            var telefonKodlari2 = new Dictionary<int, string>()
+            {
+                {302,"Konya" },
+                {421,"Elazığ" },
+                {466,"Art" }
+            };
+
+            //Ekleme 
+            telefonKodlari2.Add(322, "Kahramanmaraş");
+            telefonKodlari2.Add(321, "Osmaniye");
+            telefonKodlari2.Add(212, "İstanbul");
+            telefonKodlari2.Add(211, "Edirne");
+
+            //Değiştirme
+            telefonKodlari2[321] = "Artvin";
+
+            //Contains Key anahtar sorgusu
+            if (!telefonKodlari2.ContainsKey(321))
+            {
+                Console.WriteLine("\a Artvin'nin kod bilgisi yok");
+                telefonKodlari2.Add(321, "Bolu");
+                Console.WriteLine("Yeni kod eklendi");
+            }
+
+            //Contains Value value değer sorgusu
+            if (!telefonKodlari2.ContainsValue("İstanbul"))
+            {
+                Console.WriteLine("\a İstanbul ' un kod bilgisi yok.");
+                telefonKodlari2.Add(212, "İstanbul");
+                Console.WriteLine("Yeni kod eklendi");
+            }
+
+            //Erişme 
+            foreach (var s in telefonKodlari2)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ 28-Sorted-Dict/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _28_Sorted_Dict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dict ile aynı özellik ekstra eklemede sıralama işlemi de yapılır.
+            var kitapIndeks = new SortedDictionary<string, List<int>>()
+            {
+                { "HTML",new List<int>() { 8,10,12}},
+                {"CSS",new List<int>() { 70,80,90}},
+                {"JQUERY",new List<int> { 3,5,15} },
+                {"SQL",new List<int>{70,80} }
+            };
+            
+            kitapIndeks.Add("FTP",new List<int> { 1, 2, 3 });
+            kitapIndeks.Add("ASP.NET", new List<int>() { 50, 60 });
+        
+            foreach (var kavram in kitapIndeks)
+            {
+                Console.WriteLine(kavram.Key);
+                foreach (int s in kavram.Value)
+                {
+                    Console.WriteLine($"\t > {s,-5} pp");
+                }
+                kavram.Value.ForEach(s => Console.WriteLine("\t " + s));
+            }
+          
+
+        }
+    }
+}
