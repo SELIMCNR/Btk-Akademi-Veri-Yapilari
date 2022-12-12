145f8c2dacc69eda9f1cf55f041c5f7c9f78e5f4
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:53:19 +0300

    C#-Algorıthm
    C#-Algorıthm

---
+++ 21-SortedLıst/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+
+namespace _21_SortedLıst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedList = temeller kolleksiyonlardan
+            //Yöntem1
+            /*
+            var list = new SortedList();
+            list.Add(1, "bir");
+            list.Add(2,"iki") ;
+            */
+            //Yöntem2 tanımlama
+            var list = new SortedList()
+            {{1,"bir"},
+                {2,"iki" },
+                {3,"Üç" },
+                {8 ,"Sekiz"},
+                     {6 ,"altı"},
+                          {5 ,"beş"}
+            };
+            list.Add(4, "Dört");
+            list.Remove(3); //silme işlemi
+            //Dolaşma
+            foreach (DictionaryEntry item in list)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+            Console.WriteLine("Listedeki eleman sayısı :{0} ",list.Count);
+            Console.WriteLine("Listenin kapasitesi :{0} ",list.Capacity);
+            list.TrimToSize();//kapasiteyi set eder. Listedeki eleman sayısı ile böler.
+            Console.WriteLine("Listenin kapasitesi :{0} ",list.Capacity);
+
+            //Erişme
+            //Key
+            Console.WriteLine(list[4]);
+            //ındex
+            Console.WriteLine(list.GetByIndex(0));
+            //Get key metodu
+            Console.WriteLine(list.GetKey(0));
+            //Liste sonundaki elemanın değeri
+            Console.WriteLine(list.GetByIndex(list.Count-1));
+            //Anahtarlar dolaşımı
+            var anahtarlar = list.Keys;
+            Console.WriteLine("\n Anahtarlar");
+            foreach (var item in anahtarlar)
+            {
+                Console.WriteLine(item);
+            }
+            //Değerler dolaşımı
+            var degerler = list.Values;
+            Console.WriteLine("\n Değerler");
+            foreach (var item in degerler)
+            {
+                Console.WriteLine(item);
+            }
+            if (list.ContainsKey(1)) //containskey o değer anahtar var mı 
+            {
+                list[1] = "One";
+            }
+            //Dolaşma 
+            foreach (DictionaryEntry item in list)
+            {
+                Console.WriteLine($"{item.Key}-{item.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 22-SortedListUygulaması/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace _22_SortedListUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedList
+            var kitapIcerigi = new SortedList();
+            kitapIcerigi.Add(1, "Önsöz");
+            kitapIcerigi.Add(50, "Değişkenler");
+            kitapIcerigi.Add(40, "Operatörler");
+            kitapIcerigi.Add(60, "Döngüler");
+            kitapIcerigi.Add(45, "İlişkisel Operatörler");
+            
+            Console.WriteLine("İçindekiler");
+            Console.WriteLine(new String ('-',25));
+
+            Console.WriteLine($"{"Konular",-30} {"Sayfalar",5}");
+            foreach (DictionaryEntry item in kitapIcerigi)
+            {
+                Console.WriteLine($"{item.Value,-30} {item.Key,5}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
