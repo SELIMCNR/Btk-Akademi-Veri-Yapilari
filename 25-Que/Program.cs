e37d6543eb63a3f7e656f4e881fbc99a014f3bba
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:55:38 +0300

    C#-Algorıthm
    C#-Algorıthm

---
+++ 25-Que/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _25_Que
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sesliHarfler = new List<char>()
+            {
+                'a','e','ı','i','u','ü','o','ö'
+            };
+
+            ConsoleKeyInfo secim;
+            var kuyruk = new Queue<char>();
+
+            foreach (char k in sesliHarfler)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{k,-5} kuyruğa eklensin mi? [e/h]");
+                Console.ReadKey();
+                secim = Console.ReadKey();
+                Console.WriteLine();
+                if(secim.Key == ConsoleKey.E)
+                {
+                    kuyruk.Enqueue(k);
+                    Console.WriteLine($"\n{k,-5} kuyruğa eklendi.");
+                    Console.WriteLine($"Kuyruktaki eleman sayısı: {kuyruk.Count}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Kuyruktan elemanlarin kaldırılması işlemi için Esc tuşuna basınız.");
+            secim = Console.ReadKey();
+   
+           if(secim.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine();
+                while (kuyruk.Count > 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{kuyruk.Peek(),5} kuyruktan çıkartılıyor.");
+                    Console.WriteLine($"{kuyruk.Dequeue(),5} kuyruktan çıkarıldı.");
+                    Console.WriteLine($"Kuyruktaki eleman sayisi: {kuyruk.Count}");
+                }
+                Console.WriteLine("\n Kuyruktan çıkarma işlemi tamamlandı.");
+
+            }
+
+            Console.WriteLine("Program bitti ");
+            
+            quetemelleri();
+            Console.ReadKey();
+
+        }
+
+        private static void quetemelleri()
+        {
+            // Que Kuyruj system collec generic boxing unboxing yok 
+            // t-> type->
+            //First-in First-Out(Fifo). ilk gelen ilk çıkar
+            //enqueue,dequeue,peek,count,clear
+
+            //Sesli Harfler yazan program
+            var karakterKuyruğu = new Queue<char>();//sys-collec-generic 
+            //ekleme
+            karakterKuyruğu.Enqueue('a');
+            karakterKuyruğu.Enqueue('b');
+            karakterKuyruğu.Enqueue('e');
+            Console.WriteLine($"Eleman sayısı: {karakterKuyruğu.Count}");
+
+            var dizi = karakterKuyruğu.ToArray(); // que diziye ekleme
+
+            //Çıkarma
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyruğu.Peek()}");
+            Console.WriteLine($"{karakterKuyruğu.Dequeue()} kuyruktan çıkartıldı.");
+            Console.WriteLine($"Eleman sayisi: {karakterKuyruğu.Count}");
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyruğu.Peek()}");
+        }
+    }
+}
---
+++ 26-Linked-List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _26_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System collections.Generic sınıfına aittir 
+            //boxin unboxing yok generic yapıda-> biz bildiriyoruz.
+            //Doğrusal veri yapısıdır.İlk düğüm first son düğüm last , first.next bir sonraki düğüm
+            //Last.previous ile sondan bir önceki düğüm gibi çalışır.s
+            //add first ile ilk elemana ekleme , add last son eleman ekleme
+            //Add before, Add after Next/preveous value özellikleri de var.
+
+            //LinkedList<T> Temelleri 
+            //Tanımlama
+            var sehirler = new LinkedList<string>();
+            sehirler.AddFirst("Kahramanmaraş"); // 0 ındis kahramanmaraş
+            sehirler.AddFirst("Trabzon");  // 1 indis Tranbzon
+            sehirler.AddLast("İstanbul"); // en son indise İstanbul at
+            
+            sehirler.AddAfter(sehirler.Find("Kahramanmaraş"),"Samsun"); // Kahramanmaraşı bul ondan sonra Samsun ekle
+            sehirler.AddBefore(sehirler.First.Next.Next, "Giresun"); // ordudan sonra yani 3.indis
+            sehirler.AddAfter(sehirler.Last.Previous, "Sinop"); // Samsundan sonra yani 4.indis
+            sehirler.AddAfter(sehirler.Find("Sinop"), "Zonguldak");
+            Console.WriteLine();
+            Console.WriteLine("Gidiş Güzergahı");
+            
+            var eleman = sehirler.First; 
+            while (eleman!= null)
+            {
+                Console.WriteLine(eleman.Value);
+                eleman = eleman.Next;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dönüş Güzergahı");
+            Console.WriteLine();
+
+            var gecici = sehirler.Last; //listeyi tersten yazdırır
+            while (gecici != null)
+            {
+                Console.WriteLine(gecici.Value);
+                gecici = gecici.Previous; //listeyi tersten yazdırır
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
