12a779516ff6d3d230f62ee86750831d2fa92923
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:41:41 +0300

    C#-Algortıhm
    C#-Algortıhm

---
+++ 10-ArrayLıst/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+//Array List tanımlama
+
+var arrayList = new ArrayList();
+
+// Tanım ve ekleme
+var arrayList1 = new ArrayList(){  // liste oluşturma ve ekleme
+    10,"Btk Akademi",true,"e"
+};
+
+// ekleme yapma
+arrayList.Add(10);
+arrayList.Add("BTK Akademi");
+arrayList.Add(true);
+arrayList.Add("e");
+
+int[] sayilar = new int[] { 23, 44, 55 };
+arrayList.AddRange(sayilar); // listeye diziden rastgele sayı eklme
+
+//Dolaşma 
+Console.WriteLine();
+foreach (var e in arrayList)
+{
+    Console.Write($"{e}");
+}
+Console.WriteLine();
+Console.WriteLine(arrayList[4]); // elemana erişim
+
+
+
+
+
+
+// elemana erişim - atama
+int x = (int)arrayList[0];
+Console.WriteLine(x + 10);
+Console.ReadKey();
+
+// Elaman silme 
+arrayList.Remove(10); // içine yazılan elemanı siler 
+arrayList.RemoveAt(1); // indise göre siler
+arrayList.RemoveRange(3, 3); // 3.indisten başla 3 eleman sil  
+//Dolaşma 
+Console.WriteLine();
+foreach (var e in arrayList)
+{
+    Console.Write($" {e} ");
+}
+
---
+++ 11-Lıst/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 15;
+int [] Seri = new int[] { 70, 80, 90 }; // seri eleman dizisi
+   // List Tanımlama // referans tipli veri 
+List<int> numbers = new List<int>();  // referans tipli verilerde new kullanılır.
+  //2.yöntem list tanımlama
+var sayilar = new List<int>();
+
+//3.yöntem tanımlama ve ekleme
+var numbers1 = new List<int>() { 10, 15, 20 };
+
+// ekleme yapma
+sayilar.Add(10);
+sayilar.Add(15);
+sayilar.Add(20);
+sayilar.Add(x); // değişken ekledik
+sayilar.AddRange(Seri); // diziyi listeye ekledik
+sayilar.AddRange(new int[] { 40, 50, 60, 70, 80 });
+
+// araya ekleme yapma
+sayilar.Insert(3, 0); // 3. indise 0 elemeanını ekle
+sayilar.InsertRange(4, new int[] { 80, 90 }); // 4.indsten itibaren dizi elemanlarını rastgele ekle
+
+//Listeden eleman silme
+sayilar.RemoveAt(3);
+sayilar.RemoveAt(sayilar.IndexOf(10));//Dinamik silme işlemi 55.indekste elemanı sil
+                                      // dolaşma
+foreach (int n in sayilar)
+{
+    Console.Write($"{n,-5}");
+}
+
+
+
+Console.ReadKey();
+
+
