79a9918055702d61bd22ab8ec165344c7c7c3293
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-12-12 14:57:28 +0300

    C#-Algorıthm
    C#-Algorıthm

---
+++ 29-Sorted-Set/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _29_Sorted_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedSet Küme işlemleri 
+            var A = new SortedSet<int> (RastgeleSayiUret(100)); 
+            var B = new SortedSet<int> (RastgeleSayiUret(100));
+            #region yazdırma
+            foreach (int s in A)
+            {
+                Console.Write($"{s,5}");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine();
+
+            Console.WriteLine("B kümesi");
+            foreach (int s in B)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+            #endregion
+
+            // union birleşim
+            // A.UnionWith(B);  // birleşimi verir
+            // A.IntersectWith(B); //kesişimi verir
+            // A.ExceptWith(B); // Sade a da olan yani A fark b
+            A.SymmetricExceptWith(B);
+
+            Console.WriteLine();
+            Console.WriteLine("\n\n ");
+            foreach (int s in A)
+            {
+                Console.WriteLine($"{s,5} kesişim dışında elemanlar");
+            }
+            Console.WriteLine("Toplam sayisi: {0}",A.Count);
+            Console.WriteLine();
+            Console.ReadKey();
+            SortedSet2sample();
+            sortedSetTemel();
+
+        }
+        static List <int> RastgeleSayiUret(int n)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for (int i =0; i < n; i++)
+            {
+                list.Add(r.Next(0,1000));
+                
+            }
+            return list;
+
+        }
+        private static void SortedSet2sample()
+        {
+            //SortedSet
+            var sayilar = new List<int>();
+            var r = new Random(); /*Random sayılar döndürür*/
+
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+            {
+                sayilar.Add(r.Next(0, 10)); // 0 ile 10 arasında değerler ekler 
+                Console.WriteLine($"{sayilar[i],-3}");
+            }
+            Console.WriteLine();
+
+
+            // listedeki benzersiz sayıları bulmak
+            var benzersizSayiListesi = new SortedSet<int>(sayilar);
+
+            Console.WriteLine();
+            Console.WriteLine("\nBenzersiz sayilarin Listesi");
+
+            foreach (int sayi in benzersizSayiListesi)
+            {
+                Console.WriteLine($"{sayi,-3}");
+            }
+            Console.WriteLine("Benzersiz {0} sayi var.", benzersizSayiListesi.Count);
+
+         
+        }
+
+        private static void sortedSetTemel()
+        {
+            // System collections generic sınıfı altında yer alır
+            // boxing unboxing yok , generic ifade oldundan
+            // Elemanları benzersiz olmalıdır. farklı olmalı
+            /* Sıralı sıralama ekleme sırasında yer alır. 
+             * Add()-> ekleme true-false şeklinde döner.
+             * Remove()->silme 
+             * Remove Where(producate) koşul 
+             * kesişim birleşme, ayrışım,Alt küme durumu sorgulanabilir. 
+            */
+
+            //SortedSet 
+            //Tanımlama
+            var list = new SortedSet<string>();
+
+            //ekleme 
+            if (list.Add("Mehmet"))
+            {
+                Console.WriteLine("Mehmet eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Ekleme başarısız");
+            }
+            //ekleme 3lü koşul ifadesi
+            Console.WriteLine("{0}", list.Add("Ahmet") == true ? "Ahmet eklendi" : "Ekleme başarısız");
+
+            list.Add("Şule");
+            list.Add("Neslihan");
+            list.Add("Fahrettin");
+            list.Add("Fatih");
+            Console.WriteLine("\nİsimler Listesi \n");
+
+            //Remove silme 
+            list.Remove("Sule");
+            list.RemoveWhere(deger => deger.Contains("A")); /* a ile başlayanı sil */
+            list.RemoveWhere(deger => deger.StartsWith("f")); /* f ile başlayanları sil*/
+            foreach (string i in list)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Listedeki eleman sayısı : {0,5}", list.Count);
+        }
+    }
+}
---
+++ 30-Hash-Set/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _30_Hash_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System .COLLECTİON.GENERİC sınıfı altında yer alır.
+            // generic -> Boxing/uboxing yok <T> yani tip güvenliği
+
+            /* eleman benzersizdir.
+            sıralama yok farklılıkığ 
+            Küme işlemleri var 
+            A.unionwith(b) birleştirme
+            A.Intersectwith(b) Kesişim
+            A.Exceptwith(B) -> Sadece A dakiler 
+            B.Exceptwith(A) -> Sadece B dekiler
+            A.Symmetric Exceptwith(B) -> Kesişim dışındakiler
+             */
+
+            //HashSetUygulaması
+            //Tanımlama
+            var sesliHarf = new HashSet<char>(){
+                'e','ı','i','u','ü','o','ö','b'
+            };
+            KoleksiyonYazdir(sesliHarf);
+            //ekleme
+            sesliHarf.Add('a');
+            KoleksiyonYazdir(sesliHarf);
+
+            //Listeden çıkarma 
+            sesliHarf.Remove('b');
+            KoleksiyonYazdir(sesliHarf);
+
+
+            var alfabe= new List<char>();
+            for (int i = 97; i < 123; i++)
+            {
+                alfabe.Add((char)i);
+            }
+
+            KoleksiyonYazdir(alfabe);
+            //alfabe.ForEach(k => Console.WriteLine(k));
+
+
+            //Türkçede kullanılan sesli harfler
+            sesliHarf.ExceptWith(alfabe); // Sadece birinde olan
+            sesliHarf.UnionWith(alfabe); // birleşim
+            sesliHarf.IntersectWith(alfabe); // kesişim
+            sesliHarf.SymmetricExceptWith(alfabe);//kesişim dışındakiler
+            KoleksiyonYazdir(sesliHarf);
+
+
+            Console.ReadKey();
+
+            
+
+        }
+    
+        static void KoleksiyonYazdir(IEnumerable koleksiyon){
+            Console.WriteLine();
+            int i = 0;
+            foreach (char k in koleksiyon)
+            {
+                Console.WriteLine($"{k,5}");
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Eleman sayisi: {0}",i);
+        }
+    }
+}
