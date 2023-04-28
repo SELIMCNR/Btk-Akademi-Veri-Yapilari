c1e80eac10b3fa4b2c7746567d1e0c04f605c130
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-04-28 10:46:20 +0300

    Update Program.cs

--- 04-VerıSabıt/Program.cs
+++ 04-VerıSabıt/Program.cs
@@ -6,42 +6,51 @@ using System.Threading.Tasks;
 
 namespace _04_VerıSabıt
 {
-    internal class Program
+    //Char karakterleri tutar  
+/*
+ -ASCII  : American Standart Code for Information 
+    Interchange
+ - 7-bit  2 li sayı sistemi  -0 , -1 olarak tutar bilgisayar kodları . 128 karakter var ASCII DE ORNEK: 0-9 rakamlar   a-z  , A-Z  .,_?{}() GİBİ .
+    her bit 0-1 olarak tutulur bellekte.
+    8bit  1byte , 1024 byte 1kb , 1024kb 1mb , 1024mb 1 gb , 1024gb 1 tb
+ 
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var ifade = "Merhaba programlama dünyası.";
+        Console.WriteLine(ifade);
+        Console.WriteLine(ifade.Length); //Karakter uzunluğu alır
+        Console.WriteLine(ifade.ToUpper()); //Büyük harfle yazar
+        Console.WriteLine(ifade.ToLower()); //Küçük harfle yazar
+        Console.WriteLine(ifade.TrimStart()); //Baştaki boşlukları sil
+        Console.WriteLine(ifade.TrimEnd()); //Sondaki boşlukları sil
+        Console.WriteLine(ifade.Trim()); //Hem baş hem sondaki karakterleri sil 
+        Console.WriteLine(ifade[0]);
+        Console.WriteLine(ifade[1]);
+        Console.WriteLine(ifade[2]);
+        Console.WriteLine(ifade[ifade.Length - 1]);
+
+        ortuluDegisken();
+        Console.ReadKey();
+        static void ortuluDegisken()
         {
 
-            var ifade1 = "Merhaba programlama dünyası";
-            Console.WriteLine(ifade1);        // program direk çalıştı
-            Console.WriteLine(ifade1.Length); // program uznluğu
-            Console.WriteLine(ifade1.ToUpper()); //programın hepsi büyük
-            Console.WriteLine(ifade1.ToLower()); //programın hepsi küçük
-            Console.WriteLine(ifade1.TrimStart());// baştan böl
-            Console.WriteLine(ifade1.TrimEnd()); // sondan böl 
-            Console.WriteLine(ifade1[0]);// 0.karakter
-            Console.WriteLine(ifade1[1]);
-            Console.WriteLine(ifade1[2]);
-            Console.WriteLine(ifade1[ifade1.Length-1]); // sondan bir önceki karakter
-            ortuluDegısken();
+            //Console.ReadLine() dışardan değer alır. 
+            string ifade = Console.ReadLine();
 
-            dısardanVeri();
-            Console.ReadKey();
+            //Örtülü değişken var : Çalışma zamnında runtime da değerine karar verilir. 
+            var ifade2 = Console.ReadLine();
 
-        }
+            //Read key karaktere tıklama anı
+            var ifade3 = Console.ReadKey();
+            Console.WriteLine(ifade3.Key);
+            Console.WriteLine(ifade3.KeyChar);
 
-        private static void ortuluDegısken()
-        {
-            //Ortulu degisken
-            var ifade = Console.ReadKey();
-            Console.WriteLine(ifade.Key); // yazılan değerin büyük harfini getirir
-            Console.WriteLine(ifade.KeyChar); // yazılan değeri getirir.
-        }
-
-        private static void dısardanVeri()
-        {
-            //Dışardan veri alma string değer döndürür
-            var ifade = Console.ReadLine();
             Console.WriteLine(ifade);
         }
     }
+}
 }
