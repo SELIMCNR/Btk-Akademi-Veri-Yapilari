59b7ef81db27b6c547bb0394c1b7eed276c12979
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-04-28 10:25:17 +0300

    Update Program.cs

--- 03-VerıDegıskenler/Program.cs
+++ 03-VerıDegıskenler/Program.cs
@@ -12,53 +12,72 @@ namespace _03_VerıDegıskenler
         {
 
 
-            yorumSatırı();
-            sayıDegısken();
-            degiskenOrnek();
-            Console.ReadKey();
-        }
+        // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-        private static void yorumSatırı()
-        {
-            // Yorum Satırı 
-            /*Yorum 
-              Blogğu
-             */
-            // "C:\\user\\zcomert"; kaçış ifadesi
-            // @"C:\\user\\zcomert"
-            //\n alt satır boşluğu \t yan satır boşluk \a alarm uyarı sesi 
-            String ifade = " \n Btk Akademi Programlama Ogreniyorum";
-            Console.WriteLine(ifade);
-        }
+//Değişkenler 
+/*
+ Yerleşik tip : int , double-float,datetime,bool,char,string....
 
-        private static void sayıDegısken()
-        {
-            int sayi = 5;
-            Console.WriteLine(sayi);
-            Console.WriteLine(sayi + 2);
-            Console.WriteLine(sayi * sayi);
-            Console.WriteLine(sayi - 5);
-        }
+ Kullanıcı tarafli tipler : struct,class 
 
-        private static void degiskenOrnek()
-        {
-            // tip degiskenler
-            int sayi1;
-            double pi = 3.14;
-            char secim = 'e';
-            String name = "Selim";
-            bool dogruMu = false;
+-isimlendirme : rakamla değişken başlamaz, # ? ! bunlar değişkende kullanılmaz ama _ kullanılır , 
+  anahtar kelimeler değişken isimlendirmesinde kullanılamaz.for,while,if,using,enum vs.
+ -UpperCase  -> UzunFonksiyonAdi;
+ -camelCase  -> uzunFonksiyonAdi;
+ Türkçe karakter  ç,ğ,ı,i,u,ü dikkat edilmeli.
+
+ */
+
+//Karakter dizeleri 
+/* yorum satırı */
+// refactoring hızlı eylemler yapılır 
+// escape \n  alt satıra geç, \t boşluk bırak, \a alarm ver  kaçış ifadeleri 
+
+string ifade = "\n Btk \n Akademi \n Programlama \n Ogreniyorum.";
+string ifade1 = "\t Btk \t Akademi \t Programlama \t Ogreniyorum.";
+
+string ifade2 = "\a Btk \a Akademi \a Programlama \a Ogreniyorum.";
+
+//Dosya yolu yazımları 
+string ifade3 = "C:\\user\\zcomert";
+string ifade4 = @"C:\user\zcomert";
+
+Console.WriteLine(ifade);
 
 
-            
 
-            // tip atama 
-            string isim;
-            isim = "Ahmet";
-            string surname = "Çınar";
-            Console.WriteLine("Merhaba " + isim + " .");
-            Console.WriteLine("Merhaba Sayın " + surname + ".");
-           
-        }
+sayiOrnek();
+
+degiskenOrnek();
+Console.ReadKey();
+}
+static void degiskenOrnek()
+{
+    //tip 
+    int sayi1;
+    double s2 = 0.15;
+    float pi = 3.14f;
+    char secim = 'e';
+    string isim = "selim";
+    bool dogruMu = false;
+
+    //değer atama
+    isim = "Yusuf";
+    string soyad = "Çınar";
+
+    Console.WriteLine("Merhaba " + isim + " " + soyad + ".");
+    isim = "İbrahim";
+    Console.WriteLine("Merhaba Sayın " + isim + ".");
+}
+
+static void sayiOrnek()
+{
+    int sayi = 5;
+    Console.WriteLine(sayi);
+    Console.WriteLine(sayi + 2);
+    Console.WriteLine(sayi * sayi);
+    Console.WriteLine(sayi - 5);
+}   
     }
 }
