98a8ab915a51f77e64fba88d00fd0c3b22150bc4
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-06-01 23:02:56 +0300

    Add files via upload

---
+++ 10_Değer_Referans/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 20;
+        Console.WriteLine("{0},{1}",x,y);
+        
+        Degistir(ref x,ref y);
+        
+        Console.WriteLine("{0},{1}",x,y);
+
+        Console.ReadKey();
+    }
+
+    private static void Degistir(ref int x,ref int y)
+    {
+        int gecici = x;
+        x = y;
+        y=gecici;
+        Console.WriteLine("{0},{1}", x, y);
+    }
+}
---
+++ 11_Lists_Yapısı/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+
+var sehirler = new List<string>()
+{
+    "Ankara",
+    "İstanbul",
+    "Van",
+    "Samsun",
+    "Ordu"
+};
+/*
+foreach (string s in sehirler)
+{
+    Console.WriteLine(s);
+}*/
+
+//Lambda expression =>
+sehirler.ForEach (s => Console.WriteLine (s));
+
+Console.WriteLine(new string('-',50));
+
+var iller = sehirler;
+iller.ForEach (i => Console.WriteLine (i));
+
+
+sehirler.Add("Sinop");
+sehirler.ForEach(s => Console.WriteLine (s));
+Console.WriteLine();
+iller.ForEach(i => Console.WriteLine(i));
+
+iller.Remove("Ankara");
+Console.WriteLine();
+iller.ForEach(i=> Console.WriteLine(i));
+
+Console.WriteLine();
+sehirler.ForEach((s) => Console.WriteLine (s));
+
+
+
+Console.ReadKey();
